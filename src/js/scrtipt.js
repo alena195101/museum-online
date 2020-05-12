16bf636b9a3f7b548a41ead9920d1f027b5ede5e
alena195101 <alena195101@yandex.ru>
2020-05-12 11:46:25 +0300

    up

--- src/js/scrtipt.js
+++ src/js/scrtipt.js
@@ -1,4 +1,13 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
- 	$( ".row" ).removeClass( "row-cols-3" ).addClass( "row-cols-2" );
+document.addEventListener("DOMContentLoaded", ready );
+window.onresize = function(event) {
+    ready();
+};
 
-  }
+function ready() {
+	var window_weight = $(window).width();
+	if (window_weight <= 1000) {
+		$( ".row" ).removeClass( "row-cols-3" );
+		$( ".row" ).addClass( "row-cols-2" );
+	}
+}
+
