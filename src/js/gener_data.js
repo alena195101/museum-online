bfc61f3f6f959e0e1139581bcfab2e01ad7034db
alena195101 <alena195101@yandex.ru>
2020-05-14 20:18:11 +0300

    update

---
+++ src/js/gener_data.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", ready );
+window.onresize = function(event) {
+    ready();
+};
+
+function ready() {
+	let href = ['https://petitegalerie.louvre.fr/visite-virtuelle/saison5/','http://www.chnmuseum.cn/Portals/0/web/vr/2018dtfh/',''];
+	let name = ['Лувр','Национальный <br>музей Китая','Токийский <br>национальный музей'];
+	let img = ['luvr.jpeg','chinanat.jpg','tokyonat.jpg'];
+	for(let i = 0; i < href.length; i++ ){
+		document.getElementById("gal_row").innerHTML += "<div class='col col_ph'><div class = 'div_photo'><a class ='photo_t' href = " + href[i] + "><img class = 'photo' src=src/img/" +img[i]+ "><h3 class='img_descript'> Обзор</h3></a></div><div class='photo_text'><h2>"+name[i]+"</h2></div></div>";
+
+	}
+	}
+
